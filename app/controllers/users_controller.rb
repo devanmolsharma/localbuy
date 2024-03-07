a0f63a4bebc65d473d7dd4d969ac392226508403
Anmol Sharma <keepupsharma@gmail.com>
2024-03-06 22:43:07 -0600

    added login controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def index
+        if(session[:user_id].nil?)
+            redirect_to('/login')
+        end
+    end
+
+    def login
+
+    end
 end
